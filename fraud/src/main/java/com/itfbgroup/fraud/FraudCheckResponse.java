c2a10f9109abcd2ec986e02d2bc154c57ee43982
MaksimProsnev <yashadf@gmail.com>
2022-02-26 23:32:18 +0500

    Added second microservice + second db container

---
+++ fraud/src/main/java/com/itfbgroup/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.itfbgroup.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/itfbgroup/fraud/FraudCheckService.java
@@ -0,0 +1,23 @@
+package com.itfbgroup.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean ifFraudulentCustomer(Long customerId){
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .created(LocalDateTime.now())
+                        .build()
+        );
+        return false;
+    }
+}
