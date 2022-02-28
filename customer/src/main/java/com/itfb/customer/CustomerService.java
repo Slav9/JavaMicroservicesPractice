a74ea2827e288186131095134c0b801af2e47989
MaksimProsnev <yashadf@gmail.com>
2022-03-01 01:31:42 +0500

    Added openfeign

--- customer/src/main/java/com/itfb/customer/CustomerService.java
+++ customer/src/main/java/com/itfb/customer/CustomerService.java
@@ -1,15 +1,16 @@
 package com.itfb.customer;
 
+import com.itfb.clients.FraudCheckResponse;
+import com.itfb.clients.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService{
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -18,11 +19,9 @@ public class CustomerService{
                 .email(request.email())
                 .build();
         customerRepository.saveAndFlush(customer);
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+        FraudCheckResponse fraudCheckResponse =
+                fraudClient.isFraudster(customer.getId());
 
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("fraudster");
--- fraud/src/main/java/com/itfbgroup/fraud/FraudController.java
+++ fraud/src/main/java/com/itfbgroup/fraud/FraudController.java
@@ -1,5 +1,6 @@
 package com.itfbgroup.fraud;
 
+import com.itfb.clients.FraudCheckResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
